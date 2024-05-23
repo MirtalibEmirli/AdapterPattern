8136faa43ce24a6eacc5e774447a822b23a44e90
Amirl_kl34@itstep.edu.az <Amirl_kl34@itstep.edu.az>
2024-05-24 01:11:49 +0400

    Add project files.

---
+++ AdapterPattern/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+Tv tv = new();
+
+Device device = new Joiner(tv);
+device.TurnOn();
+
+interface Device
+{
+    public void TurnOn();
+}
+
+
+class Tv() 
+{
+    public void Join() {
+        Console.WriteLine("JOINED");
+    }
+   
+}
+
+//burda ona gore object adaptor isdifade eddimki 
+//biz Joiner i Tv den Torede bilmerik menasiz olar
+class Joiner : Device
+{
+    private Tv tv;
+
+    public Joiner(dynamic _tv)
+    {
+        if(_tv is Tv)
+        tv = _tv;
+    }
+
+    public void TurnOn()
+    {
+        tv.Join();
+    }
+}
+
+
+
